28382bd5eda60f9d48a18448bc3a41e652baa1a2
moepwizzy <marcus.boden@posteo.de>
2014-12-18 11:32:49 +0100

    work in progress...

--- libsvm_wrapper.cpp
+++ libsvm_wrapper.cpp
@@ -1,6 +1,6 @@
 #include "libsvm_wrapper.h"
 
-void libsvm::prepare() {
+bool libsvm::prepare() {
     problem->l = trainVec.size();
     problem->y = new double [trainVec.size()];
     problem->x = new svm_node* [trainVec.size()];
@@ -12,13 +12,58 @@ void libsvm::prepare() {
         }
     }
     prepared = true;
+    return generateParam();
 }
 
-void libsvm::addTrainSeq(std::vector<int> vec, int label) {
+bool libsvm::addTrainSeq(std::vector<int> vec, int label) {
+    if (prepared)
+        return false;
     trainVec.push_back(new Instance(vec,label));
+    return true;
 }
 
-void libsvm::addTestSeq(std::vector<int> vec) {
+bool libsvm::addTestSeq(std::vector<int> vec) {
+    if (prepared)
+        return false;
     testVec.push_back(new Instance(vec));
+    return true;
 }
 
+bool libsvm::generateParam() {
+    if (!prepared)
+        return false;
+    param = new svm_parameter();
+    param->svm_type = C_SVC;
+    param->kernel_type = LINEAR;
+    param->cache_size = 1024;
+    param->C = 1;
+    param->eps = 0.001;
+    param->nr_weight = 2;
+    param->weight_label = new int[2];
+    param->weight_label[0] = -1;
+    param->weight_label[1] = 1;
+    param->weight = getWeights();
+    param->probability = 1;
+    return (canBeTrained = true);
+}
+
+double* libsvm::getWeights () {
+    double* weights = new double[2];
+    for (std::vector<Instance*>::iterator it = trainVec.begin(); it != trainVec.end(); ++it)
+        (*it)->label == -1 ? weights[0]++ : weights[1]++;
+    return weights;
+}
+
+bool libsvm::train() {
+    if (!canBeTrained)
+        return false;
+    const char* str = svm_check_parameter(problem,param);
+    if (str != NULL)
+        return false;
+    model = svm_train(problem,param);
+    return true;
+}
+
+double libsvm::evaluate() {
+    
+}
--- libsvm_wrapper.h
+++ libsvm_wrapper.h
@@ -3,6 +3,8 @@
 
 #include "svm.h"
 #include <vector>
+#include <cstdlib>
+void do_nothing(const char *s) {};
 
 struct Instance {
     std::vector<int> vec;
@@ -12,48 +14,56 @@ struct Instance {
 };
 
 class libsvm {
-    svm_model model;
+    svm_model* model;
     svm_parameter* param;
     svm_problem* problem;
     bool canBeTrained;
     bool canBeTested;
     bool prepared;
     bool generateParam();
+    double* getWeights();
     std::vector<Instance*> trainVec;
     std::vector<Instance*> testVec;
 public:
-    libsvm(svm_model mod) : model(mod) {
+    libsvm(svm_model* mod) : model(mod) {
+        svm_set_print_string_function(&do_nothing);
         canBeTested = true;
         canBeTrained = false;
         prepared = false;
     };
     libsvm(svm_parameter* par) : param(par) {
+        svm_set_print_string_function(&do_nothing);
         canBeTested = false;
         canBeTrained = false;
         prepared = false;
     };
     libsvm(svm_problem* pro) : problem(pro) {
+        svm_set_print_string_function(&do_nothing);
         canBeTested = false;
         canBeTrained = false;
         prepared = false;
     };
     libsvm(svm_parameter* par, svm_problem* pro) : param(par), problem(pro) {
+        svm_set_print_string_function(&do_nothing);
         canBeTrained = true;
         canBeTested = false;
         prepared = false;
     };
     ~libsvm() {
         svm_destroy_param(param);
-        svm_free_model_content(&model);
+        svm_free_model_content(model);
         for (std::vector<Instance*>::iterator it = trainVec.begin(); it != trainVec.end(); ++it)
             delete (*it);
         for (std::vector<Instance*>::iterator it = testVec.begin(); it != testVec.end(); ++it)
             delete (*it);
     };
-    void prepare();
-    void addTrainSeq(std::vector<int>, int);
-    void addTestSeq(std::vector<int>);
+    svm_parameter* getParameter() {return param;};
+    bool prepare();
+    bool addTrainSeq(std::vector<int>, int);
+    bool addTestSeq(std::vector<int>);
+    bool train();
+    double evaluate();
+    double getAUC();
 };
-
 
 #endif
